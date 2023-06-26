25ab8a98c7f7824f0d0c0a6585a8a42ec10a7b70
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-06-26 21:46:11 +0300

    Update BrowserRouter

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/intelinvest">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       {/* <BrowserRouter> */}
       <App />
     </BrowserRouter>
