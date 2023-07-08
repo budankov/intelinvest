725aedde0e09379ffb0ab1202b6c0c5888fbefb4
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-07-08 21:03:43 +0300

    Add GoogleAuth
    Додав гугл авторизацію і стилі до неї.

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,9 +1,17 @@
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setUser } from 'redux/auth/userSlice';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from 'firebase/auth';
 import Form from 'components/Form/Form';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// Icons
+import { ReactComponent as GoogleAuth } from 'images/icons/google-icon.svg';
 
 import styles from './RegisterForm.module.scss';
 
@@ -28,6 +36,27 @@ const RegisterForm = ({ onLoginClick }) => {
       })
       .catch(() => Notify.failure('Такий користувач вже існує'));
   };
+
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleLogin = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then(({ user }) => {
+        console.log(user);
+        dispatch(
+          setUser({
+            email: user.email,
+            id: user.uid,
+            token: user.accessToken,
+          })
+        );
+        navigate('/app');
+        Notify.success(`${user.email} доброго дня`);
+      })
+      .catch(() => Notify.failure('Помилка під час авторизації через Google'));
+  };
+
   return (
     <div className={styles.registerFormContainer}>
       <h2 className={styles.formTitle}>Реєстрація</h2>
@@ -41,6 +70,12 @@ const RegisterForm = ({ onLoginClick }) => {
           Увійти
         </button>
       </p>
+      <button onClick={handleGoogleLogin} className={styles.googleAuth}>
+        <div className={styles.googleAuth__container}>
+          <GoogleAuth className={styles.googleAuth__icon} />
+          <span className={styles.googleAuth__title}>Увійти через Google</span>
+        </div>
+      </button>{' '}
     </div>
   );
 };
--- src/shared/api/firebaseApi.js
+++ src/shared/api/firebaseApi.js
@@ -1,5 +1,4 @@
 import { initializeApp } from 'firebase/app';
-import { GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,4 +10,3 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-export const googleAuthProvider = new GoogleAuthProvider();
