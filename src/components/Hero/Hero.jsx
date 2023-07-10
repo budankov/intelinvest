0247c5fc24998d4ac4c426b5cf46ad6deef073c4
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-07-10 21:26:43 +0300

    Update Hero
    Update Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,11 +4,11 @@ const Hero = () => {
   return (
     <section className={styles.hero}>
       <div className={styles.wrapper}>
-        <h1>Облік та контроль ваших інвестицій</h1>
-        <ul>
-          <li>Додайте куплені акції та облігації</li>
-          <li>Слідкуйте за портфелем</li>
-          <li> Аналізуйте прибутковість</li>
+        <h1 className={styles.hero__title}>Облік та контроль ваших інвестицій</h1>
+        <ul className={styles.hero__list}>
+          <li className={styles.hero__item}>Додайте куплені акції та облігації</li>
+          <li className={styles.hero__item}>Слідкуйте за портфелем</li>
+          <li className={styles.hero__item}> Аналізуйте прибутковість</li>
         </ul>
       </div>
     </section>
