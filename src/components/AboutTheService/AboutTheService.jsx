dd23d7fd9c9f4e3f10b3e2c58c67511e3a3419bf
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-07-11 22:48:46 +0300

    Update AboutTheService
    Додав блок AboutTheService

--- src/components/AboutTheService/AboutTheService.jsx
+++ src/components/AboutTheService/AboutTheService.jsx
@@ -1,77 +1,25 @@
+import AppStoreLogo from '../../images/home-page/about-the-service/apple-store.svg';
+import GooglePlayLogo from '../../images/home-page/about-the-service/google-play-store.svg';
+
 import styles from './AboutTheService.module.scss';
 
 const AboutTheService = () => {
   return (
-    <div id="about-the-service" className={styles.wrapper}>
-      <h2>AboutTheService</h2>
-      <p>
-        Стандартний Lorem Ipsum, використовуваний з XVI ст. "Lorem ipsum dolor
-        sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum." Розділ 1.10.32 "de Finibus Bonorum et Malorum", написаного
-        Цицероном у 45 році до н.е. "Sed ut perspiciatis unde omnis iste natus
-        error sit voluptatem accusantium doloremque laudantium, totam rem
-        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-        beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
-        voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
-        dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-        est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-        sed quia non numquam eius modi tempora incidunt ut labore et dolore
-        magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-        nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-        aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit
-        qui in ea voluptate velit esse quam nihil molestiae consequatur, vel
-        illum qui dolorem eum fugiat quo voluptas nulla pariatur?" Переклад
-        Х.Рекема англійською, 1914 "But I must explain to you how all this
-        mistaken idea of denouncing pleasure and praising pain was born and I
-        will give you a complete account of the system, and expound the actual
-        teachings of the great explorer of the truth, the master-builder of
-        human happiness. No one rejects, dislikes, or avoids pleasure itself,
-        because it is pleasure, but because those who do not know how to pursue
-        pleasure rationally encounter consequences that are extremely painful.
-        Nor again is there anyone who loves or pursues or desires to obtain pain
-        of itself, because it is pain, but because occasionally circumstances
-        occur in which toil and pain can procure him some great pleasure.
-      </p>
-      <br></br>
-      <p>
-        Стандартний Lorem Ipsum, використовуваний з XVI ст. "Lorem ipsum dolor
-        sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum." Розділ 1.10.32 "de Finibus Bonorum et Malorum", написаного
-        Цицероном у 45 році до н.е. "Sed ut perspiciatis unde omnis iste natus
-        error sit voluptatem accusantium doloremque laudantium, totam rem
-        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-        beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
-        voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
-        dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-        est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-        sed quia non numquam eius modi tempora incidunt ut labore et dolore
-        magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-        nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-        aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit
-        qui in ea voluptate velit esse quam nihil molestiae consequatur, vel
-        illum qui dolorem eum fugiat quo voluptas nulla pariatur?" Переклад
-        Х.Рекема англійською, 1914 "But I must explain to you how all this
-        mistaken idea of denouncing pleasure and praising pain was born and I
-        will give you a complete account of the system, and expound the actual
-        teachings of the great explorer of the truth, the master-builder of
-        human happiness. No one rejects, dislikes, or avoids pleasure itself,
-        because it is pleasure, but because those who do not know how to pursue
-        pleasure rationally encounter consequences that are extremely painful.
-        Nor again is there anyone who loves or pursues or desires to obtain pain
-        of itself, because it is pain, but because occasionally circumstances
-        occur in which toil and pain can procure him some great pleasure.
-      </p>
-    </div>
+    <section id="about-the-service" className={styles.aboutTheService}>
+      <div className={styles.wrapper}>
+        <div className={styles.aboutTheService__container}>
+          <h2 className={styles.aboutTheService__title}>Інтелектуальна заміна Excel та звітам брокера</h2>
+          <p className={styles.aboutTheService__text}>Просто додавайте угоди з акціями та облігаціями – ваш дохід розрахується автоматично.
+            Слідкуйте за вашим портфелем, вивчайте детальну аналітику та отримуйте важливі сповіщення з будь-якого пристрою.</p>
+          <p className={styles.aboutTheService__text}>З мобільним додатком портфель завжди під рукою.</p>
+          <div className={styles.appBtns}>
+            <a className={styles.appBtn} href="https://apps.apple.com/ru/app"><img className={styles.appBtn__icon} src={AppStoreLogo} alt="App Store" /><span className={styles.appBtn__iconText} >App Store</span></a>
+            <a className={styles.appBtn} href="https://play.google.com/store/apps"><img className={styles.appBtn__icon} src={GooglePlayLogo} alt="Google Play" /><span className={styles.appBtn__iconText} >Google Play</span></a>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
+
 export default AboutTheService;
