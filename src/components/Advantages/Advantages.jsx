beef24223537e9291a74901bad889084a3ee1cec
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-07-15 16:12:10 +0300

    Updare component Advantages
    Доробив компонент Advantages

--- src/components/Advantages/Advantages.jsx
+++ src/components/Advantages/Advantages.jsx
@@ -31,8 +31,41 @@ const Advantages = () => {
     }
   };
 
-  const text1 = 'Привіт світ 1';
-  const text2 = 'Привіт світ 2';
+
+  const textTime = (
+    <>
+      <span>Якщо ви торгуєте через <b>Interactive Brokers</b>, користуєтеся послугами <b>Альфа-Банку</b>, <b>Укрбізнесбанку</b>, Фінансової компанії "<b>ФІНАМ</b>", <b>Ощадбанку</b>, <b>Промінвестбанку</b>, <b>ВТБ Банку</b>, <b>Укралсибу</b>, <b>Альпарі Україна</b>, <b>Кредит-Інвест Банку</b>, <b>ITI Капітал</b> або <b>Тас-Інвест</b>, то ви можете в автоматичному режимі перенести інформацію про портфель на сервіс. Для цього достатньо завантажити звіт про угоди та завантажити його на сторінці <b>"Імпорт"</b> після реєстрації.</span><br /><br />
+      <span>Нові угоди - чи то зарахування дивідендів, чи то продаж облігацій - додаються в пару кліків з будь-якої сторінки сервісу.</span>
+    </>
+  );
+
+
+  const textAnalytics = (
+    <>
+      <span>У вашому розпорядженні детальна аналітика, можливість об'єднувати різні брокерські рахунки та ІІС в один портфель для перегляду агрегованої інформації. Графік вартості портфеля, діаграми розподілу активів (за типом, галузям).</span >
+    </>);
+
+  const textSet = (
+    <>
+      <span>Простий і зрозумілий інтерфейс дозволяє зосередитися тільки на важливій інформації.</span><br /><br />
+      <span>Гнучке налаштування відображених колонок у таблицях дозволяє контролювати те, що потрібно саме вам.</span ><br /><br />
+      <span>Приховуйте непотрібні блоки з даними.</span ></>
+  );
+
+  const textTrueIndicators = (
+    <>
+      <span>Брокер відображає лише частину даних, необхідних для керування портфелем.</span><br /><br />
+      <span>Дізнайтеся, які показники недоступні в особистому кабінеті Вашого брокера.</span>
+    </>
+  )
+
+  const textChat = (
+    <>
+      <span>Відкрийте публічний доступ до портфеля та поділіться посиланням з колегами, друзями чи передплатниками!</span><br /><br />
+      <span>Гнучке налаштування блоків, які ви хочете показати.</span><br /><br />
+      <span>Використовуйте динамічні блоки Акції, Облігації, Графік вартості, Діаграми прямо у себе на сайті або блозі!</span>
+    </>
+  )
 
   return (
     <section id="advantages" className={styles.advantages}>
@@ -40,27 +73,64 @@ const Advantages = () => {
         <div className={styles.advantages__сontainer}>
           <div className={styles.advantages__block}>
             <div className={styles.advantages__text_block}>
-              <h2 className={styles.advantages__title}>Заголовок 1</h2>
+              <div className={`${styles.advantages__icon} ${styles.time}`}></div>
+              <h2 className={styles.advantages__title}>Почніть всього за пару хвилин</h2>
               <p className={styles.advantages__text}>
-                Lorem 1 ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ullam quibusdam ducimus? Suscipit et, sit sequi tempora laudantium quaerat. Esse adipisci non repudiandae eos magni sint ullam incidunt, tenetur repellendus.
-              </p>
-              <button type="button" onClick={() => openModal(text1)}>
+                Одночасно імпортуйте всі свої угоди з брокерського терміналу або Excel. Додайте нові угоди до кількох кліків. Усе
+                решта буде розраховано автоматично.</p>
+              <button className={styles.advantages__linkMore} type="button" onClick={() => openModal(textTime)}>
                 Дізнатись більше
               </button>
             </div>
-            <div className={styles.advantages__image_block}></div>
+            <div className={`${styles.advantages__image_block} ${styles.time}`}></div>
           </div>
           <div className={styles.advantages__block}>
             <div className={styles.advantages__text_block}>
-              <h2 className={styles.advantages__title}>Заголовок 2</h2>
+              <div className={`${styles.advantages__icon} ${styles.analytics}`}></div>
+              <h2 className={styles.advantages__title}>Повний контроль над портфелем</h2>
               <p className={styles.advantages__text}>
-                Lorem 2 ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ullam quibusdam ducimus? Suscipit et, sit sequi tempora laudantium quaerat. Esse adipisci non repudiandae eos magni sint ullam incidunt, tenetur repellendus.
-              </p>
-              <button type="button" onClick={() => openModal(text2)}>
+                Спостерігайте за прибутковістю ваших паперів та їх часткою в портфелі. Вивчайте графіки, оцінюйте прибуток та комісії. Тепер кожна копійка під вашим контролем.</p>
+              <button className={styles.advantages__linkMore} type="button" onClick={() => openModal(textAnalytics)}>
                 Дізнатись більше
               </button>
             </div>
-            <div className={styles.advantages__image_block}></div>
+            <div className={`${styles.advantages__image_block} ${styles.analytics}`}></div>
+          </div>
+          <div className={styles.advantages__block}>
+            <div className={styles.advantages__text_block}>
+              <div className={`${styles.advantages__icon} ${styles.set}`}></div>
+              <h2 className={styles.advantages__title}>Налаштовуйте під себе</h2>
+              <p className={styles.advantages__text}>
+                Бажаєте бачити поточну прибутковість чи дивідендну? Діаграму чи таблицю? Гнучкий і простий інтерфейс дозволить переглядати дані, які потрібні саме вам</p>
+              <button className={styles.advantages__linkMore} type="button" onClick={() => openModal(textSet)}>
+                Дізнатись більше
+              </button>
+            </div>
+            <div className={`${styles.advantages__image_block} ${styles.set}`}></div>
+          </div>
+          <div className={styles.advantages__block}>
+            <div className={styles.advantages__text_block}>
+              <div className={`${styles.advantages__icon} ${styles.true_indicators}`}></div>
+              <h2 className={styles.advantages__title}>Прозорі дані</h2>
+              <p className={styles.advantages__text}>
+                Отримайте більше відкритих та зрозумілих показників порівняно з вашим брокером. Повні дані допоможуть приймати виважені рішення.</p>
+              <button className={styles.advantages__linkMore} type="button" onClick={() => openModal(textTrueIndicators)}>
+                Дізнатись більше
+              </button>
+            </div>
+            <div className={`${styles.advantages__image_block} ${styles.true_indicators}`}></div>
+          </div>
+          <div className={styles.advantages__block}>
+            <div className={styles.advantages__text_block}>
+              <div className={`${styles.advantages__icon} ${styles.chat}`}></div>
+              <h2 className={styles.advantages__title}>Діліться успіхами</h2>
+              <p className={styles.advantages__text}>
+                Зробіть портфель публічним та виберіть лише те, що хочете показати оточуючим. Вставте банер із показниками портфеля у ваш блог.</p>
+              <button className={styles.advantages__linkMore} type="button" onClick={() => openModal(textChat)}>
+                Дізнатись більше
+              </button>
+            </div>
+            <div className={`${styles.advantages__image_block} ${styles.chat}`}></div>
           </div>
         </div>
       </div>
@@ -70,7 +140,7 @@ const Advantages = () => {
           <div className={styles.linkMore}>
             <div>
               <p className={styles.linkMore__text}>{selectedText}</p>
-              <Button className={styles.hero__btn} type="button" onClick={clickBtnStartUse}>
+              <Button className={styles.linkMore__btn} type="button" onClick={clickBtnStartUse}>
                 Почати користуватися <span>безкоштовно</span>
               </Button>
             </div>
