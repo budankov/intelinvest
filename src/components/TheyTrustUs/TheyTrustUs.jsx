0e89ad498c1b8c10dd992d315ec9f6be69747818
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-07-17 19:16:55 +0300

    Update TheyTrustUs

--- src/components/TheyTrustUs/TheyTrustUs.jsx
+++ src/components/TheyTrustUs/TheyTrustUs.jsx
@@ -2,28 +2,30 @@ import styles from './TheyTrustUs.module.scss';
 
 const TheyTrustUs = () => {
   return (
-    <div id="they-trust-us" className={styles.wrapper}>
-      <h2 className={styles.theyTrustUs__title}>Чому нам довіряють</h2>
-      <div className={styles.theyTrustUs__list}>
-        <div className={styles.theyTrustUs__item}>
-          <h3 className={styles.theyTrustUs__subTitle}>Понад дев'ять років роботи</h3>
-          <p className={styles.theyTrustUs__text}>Понад 100 млн. долларів активів знаходиться у портфелях наших користувачів</p>
-        </div>
-        <div className={styles.theyTrustUs__item}>
-          <h3 className={styles.theyTrustUs__subTitle}>Команда професіоналів</h3>
-          <p className={styles.theyTrustUs__text}>Кожен розробник сервісу має брокерський рахунок та досвід на біржі</p>
-        </div>
-        <div className={styles.theyTrustUs__item}>
-          <h3 className={styles.theyTrustUs__subTitle}>Повна анонімність</h3>
-          <p className={styles.theyTrustUs__text}>Ми не вимагаємо жодних документів і не зберігаємо ваші персональні дані</p>
-        </div>
-        <div className={styles.theyTrustUs__item}>
-          <h3 className={styles.theyTrustUs__subTitle}>Оперативна підтримка</h3>
-          <p className={styles.theyTrustUs__text}>Поставте питання на <a href="mailto:web@intelinvest.com">web@intelinvest.com</a> і ми гарантовано відповімо вам протягом 24 годин
-          </p>
+    <section id="they-trust-us" className={styles.theyTrustUs}>
+      <div className={styles.wrapper}>
+        <h2 className={styles.theyTrustUs__title}>Чому нам довіряють</h2>
+        <div className={styles.theyTrustUs__list}>
+          <div className={styles.theyTrustUs__item}>
+            <h3 className={styles.theyTrustUs__subTitle}>Понад дев'ять років роботи</h3>
+            <p className={styles.theyTrustUs__text}>Понад 100 млн. долларів активів знаходиться у портфелях наших користувачів</p>
+          </div>
+          <div className={styles.theyTrustUs__item}>
+            <h3 className={styles.theyTrustUs__subTitle}>Команда професіоналів</h3>
+            <p className={styles.theyTrustUs__text}>Кожен розробник сервісу має брокерський рахунок та досвід на біржі</p>
+          </div>
+          <div className={styles.theyTrustUs__item}>
+            <h3 className={styles.theyTrustUs__subTitle}>Повна анонімність</h3>
+            <p className={styles.theyTrustUs__text}>Ми не вимагаємо жодних документів і не зберігаємо ваші персональні дані</p>
+          </div>
+          <div className={styles.theyTrustUs__item}>
+            <h3 className={styles.theyTrustUs__subTitle}>Оперативна підтримка</h3>
+            <p className={styles.theyTrustUs__text}>Поставте питання на <a href="mailto:web@intelinvest.com">web@intelinvest.com</a> і ми гарантовано відповімо вам протягом 24 годин
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 export default TheyTrustUs;
