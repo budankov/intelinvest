ef16ba8c49b261121c38362558c61be61bfd69b2
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-07-17 19:07:40 +0300

    Added component TheyTrustUs
    Додав компонент TheyTrustUs

--- src/components/TheyTrustUs/TheyTrustUs.jsx
+++ src/components/TheyTrustUs/TheyTrustUs.jsx
@@ -3,74 +3,26 @@ import styles from './TheyTrustUs.module.scss';
 const TheyTrustUs = () => {
   return (
     <div id="they-trust-us" className={styles.wrapper}>
-      <h2>TheyTrustUs</h2>
-      <p>
-        Стандартний Lorem Ipsum, використовуваний з XVI ст. "Lorem ipsum dolor
-        sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum." Розділ 1.10.32 "de Finibus Bonorum et Malorum", написаного
-        Цицероном у 45 році до н.е. "Sed ut perspiciatis unde omnis iste natus
-        error sit voluptatem accusantium doloremque laudantium, totam rem
-        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-        beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
-        voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
-        dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-        est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-        sed quia non numquam eius modi tempora incidunt ut labore et dolore
-        magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-        nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-        aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit
-        qui in ea voluptate velit esse quam nihil molestiae consequatur, vel
-        illum qui dolorem eum fugiat quo voluptas nulla pariatur?" Переклад
-        Х.Рекема англійською, 1914 "But I must explain to you how all this
-        mistaken idea of denouncing pleasure and praising pain was born and I
-        will give you a complete account of the system, and expound the actual
-        teachings of the great explorer of the truth, the master-builder of
-        human happiness. No one rejects, dislikes, or avoids pleasure itself,
-        because it is pleasure, but because those who do not know how to pursue
-        pleasure rationally encounter consequences that are extremely painful.
-        Nor again is there anyone who loves or pursues or desires to obtain pain
-        of itself, because it is pain, but because occasionally circumstances
-        occur in which toil and pain can procure him some great pleasure.
-      </p>
-      <br></br>
-      <p>
-        Стандартний Lorem Ipsum, використовуваний з XVI ст. "Lorem ipsum dolor
-        sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum." Розділ 1.10.32 "de Finibus Bonorum et Malorum", написаного
-        Цицероном у 45 році до н.е. "Sed ut perspiciatis unde omnis iste natus
-        error sit voluptatem accusantium doloremque laudantium, totam rem
-        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-        beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
-        voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
-        dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-        est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-        sed quia non numquam eius modi tempora incidunt ut labore et dolore
-        magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-        nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-        aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit
-        qui in ea voluptate velit esse quam nihil molestiae consequatur, vel
-        illum qui dolorem eum fugiat quo voluptas nulla pariatur?" Переклад
-        Х.Рекема англійською, 1914 "But I must explain to you how all this
-        mistaken idea of denouncing pleasure and praising pain was born and I
-        will give you a complete account of the system, and expound the actual
-        teachings of the great explorer of the truth, the master-builder of
-        human happiness. No one rejects, dislikes, or avoids pleasure itself,
-        because it is pleasure, but because those who do not know how to pursue
-        pleasure rationally encounter consequences that are extremely painful.
-        Nor again is there anyone who loves or pursues or desires to obtain pain
-        of itself, because it is pain, but because occasionally circumstances
-        occur in which toil and pain can procure him some great pleasure.
-      </p>
+      <h2 className={styles.theyTrustUs__title}>Чому нам довіряють</h2>
+      <div className={styles.theyTrustUs__list}>
+        <div className={styles.theyTrustUs__item}>
+          <h3 className={styles.theyTrustUs__subTitle}>Понад дев'ять років роботи</h3>
+          <p className={styles.theyTrustUs__text}>Понад 100 млн. долларів активів знаходиться у портфелях наших користувачів</p>
+        </div>
+        <div className={styles.theyTrustUs__item}>
+          <h3 className={styles.theyTrustUs__subTitle}>Команда професіоналів</h3>
+          <p className={styles.theyTrustUs__text}>Кожен розробник сервісу має брокерський рахунок та досвід на біржі</p>
+        </div>
+        <div className={styles.theyTrustUs__item}>
+          <h3 className={styles.theyTrustUs__subTitle}>Повна анонімність</h3>
+          <p className={styles.theyTrustUs__text}>Ми не вимагаємо жодних документів і не зберігаємо ваші персональні дані</p>
+        </div>
+        <div className={styles.theyTrustUs__item}>
+          <h3 className={styles.theyTrustUs__subTitle}>Оперативна підтримка</h3>
+          <p className={styles.theyTrustUs__text}>Поставте питання на <a href="mailto:web@intelinvest.com">web@intelinvest.com</a> і ми гарантовано відповімо вам протягом 24 годин
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
