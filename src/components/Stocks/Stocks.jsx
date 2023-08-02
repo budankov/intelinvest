f354cb0c83eb45770349f1b4fe618813103c3cf1
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-08-02 22:41:13 +0300

    Added TolTip
    Додав тултіпи до таблиці.

--- src/components/Stocks/Stocks.jsx
+++ src/components/Stocks/Stocks.jsx
@@ -5,9 +5,13 @@ import { fetchStocks, addStock, removeStock } from 'redux/stocks/opetations';
 import { selectExchangeRate } from 'redux/currencyConverter/currencyConverterSlice';
 import { getStockSuggestions } from 'shared/api/finnhubApi';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Popup from 'reactjs-popup';
 
 import styles from './Stocks.module.scss';
 import { selectCustomStyles } from './selectCustomStyles';
+
+// Icons
+import { ReactComponent as InfoIcon } from '../../images/icons/info-icon.svg'
 
 const Stocks = () => {
     const [selectedStock, setSelectedStock] = useState(null);
@@ -118,12 +122,73 @@ const Stocks = () => {
                         <th>Актив</th>
                         <th>Ціна купівлі, {currencySymbols[currentCurrency]}</th>
                         <th>Кількість</th>
-                        <th>Тепер. ціна, {currencySymbols[currentCurrency]}</th>
-                        <th>Серед. ціна, {currencySymbols[currentCurrency]}</th>
-                        <th>Тепер. дохід, %</th>
-                        <th>Сум. дохід, {currencySymbols[currentCurrency]}</th>
-                        <th>Дохідність, %</th>
-                        <th>Доля</th>
+                        <th>Серед. ціна, {currencySymbols[currentCurrency]}
+                            <Popup
+                                trigger={<InfoIcon className={styles.tooltipIcon} />}
+                                position="bottom center"
+                                on="hover"
+                            >
+                                <div className={styles.tooltipContent}>
+                                    Середня ціна відкритої позиції. Якщо акція кілька разів купувалася та продавалася, то середня ціна буде підрахована серед тих паперів, що залишилися зараз у портфелі.
+                                </div>
+                            </Popup>
+                        </th>
+                        <th>Тепер. ціна, {currencySymbols[currentCurrency]}
+                            <Popup
+                                trigger={<InfoIcon className={styles.tooltipIcon} />}
+                                position="bottom center"
+                                on="hover"
+                            >
+                                <div className={styles.tooltipContent}>
+                                    Поточна біржова вартість акції. Якщо торги завершилися, відображається ціна закриття. *Дані надаються з доріжкою. Для архівних (не торгованих) акцій поточна ціна завжди буде 0.
+                                </div>
+                            </Popup>
+                        </th>
+                        <th>Тепер. дохід, %
+                            <Popup
+                                trigger={<InfoIcon className={styles.tooltipIcon} />}
+                                position="bottom center"
+                                on="hover"
+                            >
+                                <div className={styles.tooltipContent}>
+                                    Це поточний прибуток, виражений у відсотках щодо вартості покупок відкритих на даний момент позицій. Інакше висловлюючись, це прибутковість від зміни ціни відкритої позиції.
+                                </div>
+                            </Popup>
+                        </th>
+                        <th>Сум. дохід, {currencySymbols[currentCurrency]}
+                            <Popup
+                                trigger={<InfoIcon className={styles.tooltipIcon} />}
+                                position="bottom center"
+                                on="hover"
+                            >
+                                <div className={styles.tooltipContent}>
+                                    Це сумарний прибуток за акціями за весь час роботи портфеля.
+                                </div>
+                            </Popup>
+                        </th>
+                        <th>
+                            Дохідність, %
+                            <Popup
+                                trigger={<InfoIcon className={styles.tooltipIcon} />}
+                                position="bottom center"
+                                on="hover"
+                            >
+                                <div className={styles.tooltipContent}>
+                                    Прибутковість річнихб розрахована за методом XIRR.
+                                </div>
+                            </Popup>
+                        </th>
+                        <th>Доля
+                            <Popup
+                                trigger={<InfoIcon className={styles.tooltipIcon} />}
+                                position="bottom center"
+                                on="hover"
+                            >
+                                <div className={styles.tooltipContent}>
+                                    Частка паперу порахована до сумарної вартості активів, до якої належить папір.
+                                </div>
+                            </Popup>
+                        </th>
                         <th>Видалити</th>
                     </tr>
                 </thead>
