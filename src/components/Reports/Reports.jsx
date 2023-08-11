0b9135f457d34593fb5b6401ed9b0d520b8f8f88
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-08-11 21:44:18 +0300

    Update Reports

--- src/components/Reports/Reports.jsx
+++ src/components/Reports/Reports.jsx
@@ -3,7 +3,7 @@ import styles from './Repors.module.scss'
 const Repors = () => {
     return (
         <div>
-            <h2>Reports page</h2>
+            <h2 className={styles.title}>Reports page</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At incidunt similique, aut, consequatur consequuntur dolore ex excepturi sapiente molestiae, corporis officiis! Tenetur veritatis natus recusandae corporis! Vero blanditiis deleniti officiis?</p>
         </div>
     )
